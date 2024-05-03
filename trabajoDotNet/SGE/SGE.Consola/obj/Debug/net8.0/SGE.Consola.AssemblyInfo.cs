875dec5cfdc11cf6ccadff9dabbe61f1cf1c94aa
Thiago Gauna Rubio <thiagogaunarubio@gmail.com>
2024-05-03 15:32:19 -0300

    commit

--- trabajoDotNet/SGE/SGE.Consola/obj/Debug/net8.0/SGE.Consola.AssemblyInfo.cs
+++ trabajoDotNet/SGE/SGE.Consola/obj/Debug/net8.0/SGE.Consola.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8166bb381d30a045ecab0184af773dfec1304c79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f273851286d1be092ab1d302520781a33014bcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
